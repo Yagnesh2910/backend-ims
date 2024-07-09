665f38e6bcec17ae81ecbdee6c8da0b57abc4d7f
DELL <yagneshsanku25@gmail.com>
2024-07-09 13:07:27 +0530

    first commit

---
+++ Model/userSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+    //name, email, password,userType
+    {
+        name:{
+            type: String,
+            required: true,
+        },
+        email:{
+            type: String,
+            required: true,
+        },
+        password:{
+            type: String,
+            required: true,
+        },
+        userType: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        collection: "users",
+    }
+);
+
+module.exports = mongoose.model("users", userSchema);
